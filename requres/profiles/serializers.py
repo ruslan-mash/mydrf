de2d699a939073743892bb7d146780b51bf115e8
EndesaptTest <103508859+EndesaptTest@users.noreply.github.com>
2024-10-28 20:10:35 +0300

    changes 2010

--- requres/profiles/serializers.py
+++ requres/profiles/serializers.py
@@ -18,10 +18,9 @@ class UserSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(label="first_name", max_length=500)
     last_name = serializers.CharField(label="last_name", max_length=500)
     avatar = serializers.URLField(label="avatar", max_length=200)
-    user_supports = SupportSerializer(many=True, read_only=True)
     class Meta:
         model = models.UserProfile
-        fields = ("id", "email", "first_name", "last_name", "avatar", "user_supports")
+        fields = ("id", "email", "first_name", "last_name", "avatar",)
 
 
 class FullDataSerializer(serializers.Serializer):
@@ -39,7 +38,8 @@ class FullDataSerializer(serializers.Serializer):
         return support
 
     def to_representation(self, instance):
-        return {}
+        print(UserSerializer)
+        return instance.user_profile.all()
 
 
 
